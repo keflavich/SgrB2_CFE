f07cbad2058c4de4500abcae84fe6f03d7028421
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-07-16 09:37:28 -0600

    make apj  form

--- make_apjform.py
+++ make_apjform.py
@@ -1,5 +1,5 @@
-with open('sgrb2_cores.tex','r') as fhr:
-    with open('sgrb2_cores_apjform.tex', 'w') as fhw:
+with open('cfe.tex','r') as fhr:
+    with open('cfe_apjform.tex', 'w') as fhw:
         for line in fhr:
             if 'figures/' in line:
                 fhw.write(line.replace("figures/",""))
